8e3bfd9f783cec7289c89c795fa4189e9251eb6a
Steve Montgomery <steve@chums.com>
2024-03-29 10:09:04 -0600

    feat: add better user info tooltips

--- src/components/table/UserName.tsx
+++ src/components/table/UserName.tsx
@@ -1,32 +1,48 @@
 import React from 'react';
 import {SalesOrderRow} from "../../types";
+import Tooltip from "@mui/material/Tooltip";
+import {dateCreated, dateUpdated} from "../../utils";
 
 const UserName = ({row}: { row: SalesOrderRow }) => {
     const userName = row.UserLogon === 'websites' && !row.CurrentInvoiceNo ? row.UpdatedByUser : row.UserLogon;
+    const ttTitle = (
+        <div>
+            <div>Created: {dateCreated(row)} by {row.UserLogon}</div>
+            <div>Updated: {dateUpdated(row)} by {row.UpdatedByUser}</div>
+        </div>
+    );
     if (row.isB2B) {
         return (
-            <>
-                <span className="badge bg-primary">B2B</span>
-                <span className="ms-1">{userName}</span>
-            </>
+            <Tooltip title={ttTitle}>
+                <div>
+                    <span className="badge bg-primary">B2B</span>
+                    <span className="ms-1">{userName}</span>
+                </div>
+            </Tooltip>
         )
     }
     if (row.isEDI) {
         return (
-            <>
-                <span className="badge bg-info text-light">EDI</span>
-                <span className="ms-1">{userName}</span>
-            </>
+            <Tooltip title={ttTitle}>
+                <div>
+                    <span className="badge bg-info text-light">EDI</span>
+                    <span className="ms-1">{userName}</span>
+                </div>
+            </Tooltip>
         )
     }
     if (row.isWebsite) {
         return (
-            <span className="badge bg-dark">Shopify</span>
+            <Tooltip title={ttTitle}>
+                <span className="badge bg-secondary">Shopify</span>
+            </Tooltip>
         )
     }
 
     return (
-        <span>{row.UserLogon}</span>
+        <Tooltip title={ttTitle}>
+            <span>{row.UserLogon}</span>
+        </Tooltip>
     )
 }
 export default UserName;
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,23 @@
-import {CustomerList} from "./types";
+import {CustomerList, SalesOrderRow} from "./types";
+import dayjs from "dayjs";
 
 export function customerKey(val:{ARDivisionNo: string, CustomerNo: string}):string {
     return `${val.ARDivisionNo}-${val.CustomerNo}`;
 }
 
 export const VALUE_VARIES = '--VARIES--';
+
+
+export const dateCreated = (row:Pick<SalesOrderRow, 'DateCreated'|'TimeCreated'>):string|null => {
+    if (!row.DateCreated || !row.TimeCreated || !dayjs(row.DateCreated).isValid()) {
+        return null;
+    }
+    return dayjs(row.DateCreated).add(+row.TimeCreated, 'hours').format('MM/DD/YY HH:mm')
+}
+
+export const dateUpdated = (row:Pick<SalesOrderRow, 'DateUpdated'|'TimeUpdated'>):string|null => {
+    if (!row.DateUpdated || !row.TimeUpdated || !dayjs(row.DateUpdated).isValid()) {
+        return null;
+    }
+    return dayjs(row.DateUpdated).add(+row.TimeUpdated, 'hours').format('MM/DD/YY HH:mm');
+}
